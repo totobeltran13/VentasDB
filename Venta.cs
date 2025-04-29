36bf0e48e98b11fdaa1ca36a20871d2d28925d6f
totobeltran13 <167185511+totobeltran13@users.noreply.github.com>
2025-04-28 19:09:46 -0700

    Agregar archivos de proyecto.

---
+++ Venta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VentaDB
+	{
+	internal class Venta
+	{
+		public int Id {  get; set; }
+		public string Cliente { get; set; }
+
+	}
+}
---
+++ VentasDB/Form1.cs
@@ -0,0 +1,10 @@
+namespace VentasDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
